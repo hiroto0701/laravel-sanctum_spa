4857c5707c867e9ca6be61edf52c1f2b76e04c22
hiroto <hiroto.soccer.0701@gmail.com>
2024-01-16 22:31:53 +0900

    client axios追加

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import axios from 'axios';
+
+const csrf = () => {
+  axios.get('/sanctum/csrf-cookie').then(response => {
+    console.log(response);
+  })
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div>
+    <form @submit.prevent>
+      <div>
+        <label>Eメール</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>パスワード</label>
+        <input type="password" />
+      </div>
+      <button @click="csrf" type="submit">ログイン</button>
+    </form>
+  </div>
   </main>
 </template>
